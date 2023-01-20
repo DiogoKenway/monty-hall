9b0cf1a5fade12d11761c4af8ea38bceecac621d
Diogo Alves <diogodidi2022@gmail.com>
2023-01-19 23:23:39 -0300

    remove obj

--- src/model/porta.js
+++ src/model/porta.js
@@ -42,8 +42,3 @@ class Porta {
     }
 }
 
-const p1 = new Porta(1);
-// console.log(p1.alternarSelecao());
-console.log(p1.alternarSelecao()); 
-console.log(p1.alternarSelecao()); 
-console.log(p1.alternarSelecao()); 
