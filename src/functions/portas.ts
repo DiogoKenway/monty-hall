5e514db274150080f5fa7c9fede0ad27e8e5eb6a
Diogo Alves <diogodidi2022@gmail.com>
2023-01-23 01:11:49 -0300

    adding the number of doors

--- src/functions/portas.ts
+++ src/functions/portas.ts
@@ -1,6 +1,7 @@
 import PortaModel from "../model/porta";
 
 function criarPortas(qtde: number, selecionada: number) : PortaModel[] {
+    
     return Array.from({ length: qtde }, (_, i) => {
         const numero = i + 1;
         const temPresente = numero === selecionada 
