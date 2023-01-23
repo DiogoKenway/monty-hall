85be603ce3f596d083ab65ba9e0c5de29e072316
Diogo Alves <diogodidi2022@gmail.com>
2023-01-23 00:20:12 -0300

    removing the example component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,9 @@ import PortaModel from "../model/porta";
 export default function Home() {
 
   const [p1, setP1] = useState(new PortaModel(1));
-  const [texto, setTexto] = useState("");
 
   return (
     <div style={{ display: "flex", gap: "1rem", flexDirection: "column" }}>
-      <input type="text" value={texto} onChange={e => setTexto(e.target.value)}/>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
     </div>
   )
