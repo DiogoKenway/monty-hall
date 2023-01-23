0a3b31b3c1cf9e4654ebefccd2bb569d30427fc0
Diogo Alves <diogodidi2022@gmail.com>
2023-01-23 01:10:32 -0300

    adding new file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
+import criarPortas from "../functions/portas";
 import PortaModel from "../model/porta";
 // import Presente from "@/components/Presente";
 
@@ -7,8 +8,11 @@ export default function Home() {
 
   const [p1, setP1] = useState(new PortaModel(1));
 
+  console.log(
+  criarPortas(4, 3));
+
   return (
-    <div style={{ display: "flex", gap: "1rem", flexDirection: "column" }}>
+    <div style={{ display: "flex", gap: "1rem"}}>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
     </div>
   )
