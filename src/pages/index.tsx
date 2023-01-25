fd0806bb319fd44363b2dfbdcfd3b6ebac7c2eb8
Diogo Alves <diogodidi2022@gmail.com>
2023-01-24 22:28:04 -0300

    final code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,9 @@ import Porta from "../components/Porta";
 import criarPortas, { atualizarPortas } from "../functions/portas";
 
 export default function Home() {
-
-  const [portas, setPortas] = useState(criarPortas(5, 3));
-
-  function renderizarPortas() {
-    return portas.map((porta, i) => (
-      <Porta key={porta.numero} value={porta}
-       onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
-    ))
-  }
-
   return (
     <div style={{ display: "flex", gap: "1rem"}}>
-        {renderizarPortas()}
+        <h1>Formulário de ínicio!!!</h1>
     </div>
   )
 }
