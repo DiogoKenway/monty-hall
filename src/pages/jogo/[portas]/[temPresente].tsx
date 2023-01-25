9b19b37675778f14ee887cb35f3670e8424e73a9
Diogo Alves <diogodidi2022@gmail.com>
2023-01-24 23:50:31 -0300

    final code

--- src/pages/jogo/[portas]/[temPresente].tsx
+++ src/pages/jogo/[portas]/[temPresente].tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import Porta from "../components/Porta";
-import criarPortas, { atualizarPortas } from "../functions/portas";
-import styles from "../styles/jogo.module.css";
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import criarPortas, { atualizarPortas } from "../../../functions/portas";
+import styles from "../../../styles/jogo.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function jogo() {
 
-  const [portas, setPortas] = useState(criarPortas(4, 3));
+  const router = useRouter();
+
+  const [portas, setPortas] = useState([]);
+
+  useEffect(()=> {
+    const qtdPortas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+    
+    setPortas(criarPortas(qtdPortas, temPresente));
+
+  }, [router.query])
+  
 
   function renderizarPortas() {
     return portas.map((porta, i) => (
