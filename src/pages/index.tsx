080c0853965e84a89b1dadb367e35932f4ddf8be
Diogo Alves <diogodidi2022@gmail.com>
2023-01-25 19:19:50 -0300

    adding more styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 import styles from "../styles/formulario.module.css";
 import Cartao from "../components/Cartao";
+import Link from "next/link";
 
 export default function Formulario() {
   return (
     <div className={styles.formulario}>
       <div>
-        <Cartao bgColor={"#c0392c"} />
+        <Cartao bgColor={"#c0392c"}>
+          <h1>Monty Hall</h1>
+        </Cartao>
         <Cartao />
       </div>
       <div>
-        <Cartao />
-        <Cartao bgColor={"#28a085"} />
+        <Cartao >
+
+        </Cartao>
+        <Cartao bgColor={"#28a085"} >
+          <Link href={`/jogo/4/2`} className={styles.link}>
+            <h2>Iniciar</h2>
+          </Link>
+        </Cartao>
       </div>
     </div>
   )
